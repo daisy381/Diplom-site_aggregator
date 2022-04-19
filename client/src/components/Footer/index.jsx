befd34d88b830116d780a736ae35aca9f28f0858
duis_islam <duisenbekislam6@gmail.com>
2022-04-19 11:53:55 +0600

    add footer finished home page

---
+++ client/src/components/Footer/index.jsx
@@ -0,0 +1,44 @@
+//library
+import React from 'react';
+import {Avatar} from "antd";
+
+//Style
+import {
+    MainContainer,
+    TitleContainer,
+    LogoContainer,
+    IconsContainer
+} from "./style";
+import Icon from "../Icon";
+import {LinkContainer, LogoTitle} from "../Sidebar/style";
+
+//Images
+import LogoIcon from "../../img/easily_logo.svg";
+import twitter from '../../img/components/footer/Twitter.svg';
+import instagram from '../../img/components/footer/instagram.svg';
+import facebook from '../../img/components/footer/facebook.svg';
+import linkedin from '../../img/components/footer/Linkedin.svg';
+
+
+
+function Footer() {
+    return (
+        <MainContainer>
+            <TitleContainer>IITU Creative Agency</TitleContainer>
+            <LogoContainer>
+                <LinkContainer to="#">
+                    <Icon path={LogoIcon} width={40} height={40}/>
+                    <LogoTitle>{"Easily"}</LogoTitle>
+                </LinkContainer>
+            </LogoContainer>
+            <IconsContainer>
+                <Avatar src={twitter}/>
+                <Avatar src={instagram}/>
+                <Avatar src={facebook}/>
+                <Avatar src={linkedin}/>
+            </IconsContainer>
+        </MainContainer>
+    );
+}
+
+export default Footer;
--- client/src/pages/Home/AchievementSection/style.jsx
+++ client/src/pages/Home/AchievementSection/style.jsx
@@ -1,10 +1,12 @@
 import styled from "styled-components";
+import {Button} from "antd";
 
 
 export const MainContainer = styled.div`
   display: flex;
   justify-content: space-between;
   padding: 40px;
+  gap:30px;
 `;
 
 export const CardContainer = styled.div`
@@ -21,6 +23,9 @@ export const CardContainer = styled.div`
 export const DescriptionContainer = styled.div`
   display: flex;
   flex-direction: column;
+  width: 381px;
+  height: 322px;
+  margin-top: 30px;
 `;
 
 export const TitleContainer = styled.div`
@@ -40,11 +45,37 @@ export const TextContainer = styled.div`
   color: #0A093D;
 `;
 
-export const TextBodyContainer = styled.div`
+export const MemberContainer = styled.div`
   font-family: 'Inter';
   font-style: normal;
   font-weight: 500;
   font-size: 16px;
   line-height: 18px;
   color: #5D5B70;
+`;
+
+export const DescTitleContainer = styled.div`
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 36px;
+  line-height: 48px;
+  color: #0A093D;
+  margin-bottom: 30px;
+
+`;
+
+export const TextBodyContainer = styled.div`
+  font-family: 'Inter';
+  font-style: normal;
+  font-weight: 400;
+  font-size: 16px;
+  line-height: 26px;
+  color: #8C8C8C;
+  margin-bottom: 30px;
+`;
+
+export const CardButtonContainer = styled(Button)`
+   border: none;
+   text-align: start;
 `;
